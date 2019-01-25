a1bfff5c1758c18887c9499bf777d4dd73c9018d
Ivy Chen <ivychen@Ivys-MacBook-Pro.local>
2019-01-25 14:55:38 -0500

    Commit Test

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi there</h1>
+    <p>I'm Ivy Chen, an User experience designer</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
